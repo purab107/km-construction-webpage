568b9afb0a5e6746842e9f77c02ff58f68a62422
Purab Sahare <purabsahare@gmail.com>
2025-06-21 23:29:02 +0530

    page-fix-in-progress

--- script.js
+++ script.js
@@ -7,3 +7,23 @@ window.addEventListener('scroll', () => {
     navbar.classList.remove('scrolled');
   }
 });
+
+
+ const sections = document.querySelectorAll(".fade-section");
+
+  const observer = new IntersectionObserver(
+    entries => {
+      entries.forEach(entry => {
+        if (entry.isIntersecting) {
+          entry.target.classList.add("visible");
+        }
+      });
+    },
+    {
+      threshold: 0.1,
+    }
+  );
+
+  sections.forEach(section => {
+    observer.observe(section);
+  });
